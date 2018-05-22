84e023e63011f064647c6e5a2b4ba381e7b826e7
Samuel Gray <sam.gray453@gmail.com>
2018-05-22 17:37:18 +0100

    commit

--- js/script.js
+++ js/script.js
@@ -1,103 +1,176 @@
-$( document ).ready(function() {
-  $(".project-link-type").click(function() {
-      if ( $(this).attr('data-type') === 'app' ) {
-        $('.project').css('display', 'none')
-        $('.app').css('display', 'block')
+var html = "";
+var number = 0;
+    $.ajax({
+       type: "GET",
+       url: "js/data.json",
+       dataType: "json",
+       cache: false,
+       contentType: "application/json",
+       success: function(data) {
+        $.each(data, function(i, item) {
+          html = '<div ' + data[i].attr + ' class="project col col-3 ' + data[i].type +'">';
+          html += '<a href="#"></a>';
+          html += '<img src="'+ data[i].image + '"alt="" onerror="this.src="http://digitalpolymer.com/image/cache/catalog/no_image-1024x1269.jpg"">';
+          html += '<h2>'+ data[i].project + '</h2>';
+          html += '<h3 class="name">'+ data[i].person +'</h3>'
+          html += '<p>' + data[i].short + '</p></div>'
+          $('.project-div').append(html);
 
-} else if ($(this).attr('data-type') === 'installation') {
-    $('.project').css('display', 'none')
-    $('.installation').css('display', 'block')
-} else if ($(this).attr('data-type') === 'graphic') {
-    $('.project').css('display', 'none')
-    $('.graphic').css('display', 'block')
-} else if ($(this).attr('data-type') === 'website') {
-    $('.project').css('display', 'none')
-    $('.website').css('display', 'block')
-} else if ($(this).attr('data-type') === 'all') {
-    $('.project').css('display', 'block')
-}
-});
+          $('.project').click(function(e){
+            e.preventDefault();
+            $(".project-info").css("opacity", "1");
+            $(".project-info").css("top", "50%");
+            $(".project-info").css("left", "50%");
+            $("body").css("overflow", "hidden");
+            $(".overlay").css("opacity", "0.5");
+            $(".overlay").css("visibility", "visible");
+            if (this.hasAttribute("data-sam")) {
+              number = 0;
+             }
+             else if (this.hasAttribute("data-adam")) {
+               number = 1;
+             } else if (this.hasAttribute("data-katie")) {
+               number = 2;
+             } else if (this.hasAttribute("data-becky")) {
+               number = 3;
+             } else if (this.hasAttribute("data-abbieg")) {
+               number = 4;
+             } else if (this.hasAttribute("data-abbien")) {
+               number = 5;
+             } else if (this.hasAttribute("data-amyp")) {
+               number = 6;
+             } else if (this.hasAttribute("data-matt")) {
+               number = 7;
+             } else if (this.hasAttribute("data-verity")) {
+               number = 8;
+             } else if (this.hasAttribute("data-tessa")) {
+               number = 9;
+             } else if (this.hasAttribute("data-amyw")) {
+               number = 10;
+             }
+            $(".project-info-img").attr("src", data[number].image);
+            $(".project-info-title").text(data[number].project);
+            $(".project-info-name").text(data[number].person);
+            $(".project-info-words").text(data[number].long);
+            $(".project-info-email").text(data[number].email);
+            $(".project-info-email").attr('href', "mailto:" + data[number].email);
+
+          });
+
+})
+      },
+        error: function(xhr, status, error) {
+            console.log("opps");
+        }
+    });
+
+
+    $( document ).ready(function() {
+      $(".project-link-type").click(function() {
+          $('.project').css('display', 'none')
+          if ( $(this).attr('data-type') === 'app' ) {
+            $('.app').css('display', 'block')
+          } else if ($(this).attr('data-type') === 'installation') {
+            $('.installation').css('display', 'block')
+          } else if ($(this).attr('data-type') === 'graphic') {
+            $('.graphic').css('display', 'block')
+          } else if ($(this).attr('data-type') === 'website') {
+            $('.website').css('display', 'block')
+          } else if ($(this).attr('data-type') === 'all') {
+            $('.project').css('display', 'block')
+          } else if ($(this).attr('data-type') === 'other') {
+            $('.other').css('display', 'block')
+          }
+    });
 
 
 
-$(".scroll").click(function(e) {
-    e.preventDefault();
-    var aid = $(this).attr("href");
-    $('html,body').animate({scrollTop: $(aid).offset().top},'slow');
-});
+    $(".scroll").click(function(e) {
+        e.preventDefault();
+        var aid = $(this).attr("href");
+        $('html,body').animate({scrollTop: $(aid).offset().top},'slow');
+    });
 
 
-$(window).scroll(function(){
-    if ($(window).scrollTop() >= 50) {
-        $('nav').addClass('fixed-nav');
+    $(window).scroll(function(){
+        if ($(window).scrollTop() >= 50) {
+            $('nav').addClass('fixed-nav');
+        }
+        else {
+            $('nav').removeClass('fixed-nav');
+        }
+    });
+
+
+      $(document).click(function(e) {
+        if ($(".project-info").css("opacity") === "1") {
+          $(".project-info").css("left", "-100px");
+          $(".project-info").css("opacity", "0");
+          $("body").css("overflow", "scroll");
+          $(".overlay").css("opacity", "0");
+          $(".overlay").css("visibility", "hidden");
+        }
+      });
+    });
+
+
+    var colors = new Array(
+      [62,35,255],
+      [60,255,60],
+      [255,35,98],
+      [45,175,230],
+      [255,0,255],
+      [255,128,0]);
+
+    var step = 0;
+    //color table indices for:
+    // current color left
+    // next color left
+    // current color right
+    // next color right
+    var colorIndices = [0,1,2,3];
+
+    //transition speed
+    var gradientSpeed = 0.002;
+
+    function updateGradient()
+    {
+
+      if ( $===undefined ) return;
+
+    var c0_0 = colors[colorIndices[0]];
+    var c0_1 = colors[colorIndices[1]];
+    var c1_0 = colors[colorIndices[2]];
+    var c1_1 = colors[colorIndices[3]];
+
+    var istep = 1 - step;
+    var r1 = Math.round(istep * c0_0[0] + step * c0_1[0]);
+    var g1 = Math.round(istep * c0_0[1] + step * c0_1[1]);
+    var b1 = Math.round(istep * c0_0[2] + step * c0_1[2]);
+    var color1 = "rgb("+r1+","+g1+","+b1+")";
+
+    var r2 = Math.round(istep * c1_0[0] + step * c1_1[0]);
+    var g2 = Math.round(istep * c1_0[1] + step * c1_1[1]);
+    var b2 = Math.round(istep * c1_0[2] + step * c1_1[2]);
+    var color2 = "rgb("+r2+","+g2+","+b2+")";
+
+     $('#gradient').css({
+       background: "-webkit-gradient(linear, left top, right top, from("+color1+"), to("+color2+"))"}).css({
+        background: "-moz-linear-gradient(left, "+color1+" 0%, "+color2+" 100%)"});
+
+      step += gradientSpeed;
+      if ( step >= 1 )
+      {
+        step %= 1;
+        colorIndices[0] = colorIndices[1];
+        colorIndices[2] = colorIndices[3];
+
+        //pick two new target color indices
+        //do not pick the same as the current one
+        colorIndices[1] = ( colorIndices[1] + Math.floor( 1 + Math.random() * (colors.length - 1))) % colors.length;
+        colorIndices[3] = ( colorIndices[3] + Math.floor( 1 + Math.random() * (colors.length - 1))) % colors.length;
+
+      }
     }
-    else {
-        $('nav').removeClass('fixed-nav');
-    }
-});
 
-
-
-});
-
-
-var colors = new Array(
-  [62,35,255],
-  [60,255,60],
-  [255,35,98],
-  [45,175,230],
-  [255,0,255],
-  [255,128,0]);
-
-var step = 0;
-//color table indices for:
-// current color left
-// next color left
-// current color right
-// next color right
-var colorIndices = [0,1,2,3];
-
-//transition speed
-var gradientSpeed = 0.002;
-
-function updateGradient()
-{
-
-  if ( $===undefined ) return;
-
-var c0_0 = colors[colorIndices[0]];
-var c0_1 = colors[colorIndices[1]];
-var c1_0 = colors[colorIndices[2]];
-var c1_1 = colors[colorIndices[3]];
-
-var istep = 1 - step;
-var r1 = Math.round(istep * c0_0[0] + step * c0_1[0]);
-var g1 = Math.round(istep * c0_0[1] + step * c0_1[1]);
-var b1 = Math.round(istep * c0_0[2] + step * c0_1[2]);
-var color1 = "rgb("+r1+","+g1+","+b1+")";
-
-var r2 = Math.round(istep * c1_0[0] + step * c1_1[0]);
-var g2 = Math.round(istep * c1_0[1] + step * c1_1[1]);
-var b2 = Math.round(istep * c1_0[2] + step * c1_1[2]);
-var color2 = "rgb("+r2+","+g2+","+b2+")";
-
- $('#gradient').css({
-   background: "-webkit-gradient(linear, left top, right top, from("+color1+"), to("+color2+"))"}).css({
-    background: "-moz-linear-gradient(left, "+color1+" 0%, "+color2+" 100%)"});
-
-  step += gradientSpeed;
-  if ( step >= 1 )
-  {
-    step %= 1;
-    colorIndices[0] = colorIndices[1];
-    colorIndices[2] = colorIndices[3];
-
-    //pick two new target color indices
-    //do not pick the same as the current one
-    colorIndices[1] = ( colorIndices[1] + Math.floor( 1 + Math.random() * (colors.length - 1))) % colors.length;
-    colorIndices[3] = ( colorIndices[3] + Math.floor( 1 + Math.random() * (colors.length - 1))) % colors.length;
-
-  }
-}
-
-setInterval(updateGradient,10);
+    setInterval(updateGradient,10);
